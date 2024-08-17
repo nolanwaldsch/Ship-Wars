de320ce4312e3ae3107a863733b24f0aead40e4c
Nolan Waldschmidt <nobewon@gmail.com>
2024-08-16 23:36:57 -0400

    Initialization
    Adding unity project (a cube, plane, and movement script)

---
+++ ShipWars/Assets/Scripts/Movement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed = 0.01f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirection = new Vector3(xDirection, 0.0f ,zDirection);
+
+        transform.position += moveDirection * speed;
+
+
+
+    }
+}
